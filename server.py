f85c6bc4b7bd5b58f7057a5c18ae8dbfbb4eca8f
ninjaminion <geek.staum@gmail.com>
2015-01-21 17:36:24 -0600

    Server Set Up, Gives Unique QR
    So far, when the python script is run, when a user goes to
    http://localhost:8080/, the user will see a unique qr, which is
    basically a unique session number, so that later, when the actual
    calibration is done, the computer can find and diffrentiate between the
    screens.

--- server.py
+++ server.py
@@ -1,24 +1,30 @@
-import random
-import string
 import cherrypy
+import pyqrcode
+import shutil
+import os.path
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
 readyID = 0
 
 class StringGenerator(object):
-
     @cherrypy.expose
-    def index(self, length=1):
-        cherrypy.session['identifier'] = readyID
+    def index(self):
+        global readyID
+        cherrypy.session['identifier'] = str(readyID)
         readyID = readyID + 1
-
-    @cherrypy.expose
-    def display(self):
-        return cherrypy.session['identifier']
+        qr = pyqrcode.create(cherrypy.session['identifier'])
+        qr.png(cherrypy.session['identifier'] + ".png", scale=50)
+        shutil.move(cherrypy.session['identifier'] + ".png","public/" + cherrypy.session['identifier'] + ".png")
+        return '<style>body{background-color:black;}img{display: block; height: 100%; width: auto; margin-left: auto; margin-right: auto;}</style><img src="/static/' + cherrypy.session['identifier'] + ".png" + '"></img>'
 
 if __name__ == '__main__':
      conf = {
          '/': {
              'tools.sessions.on': True
+         },
+         '/static': {
+             'tools.staticdir.on': True,
+             'tools.staticdir.dir': current_dir + '/public'
          }
      }
      cherrypy.quickstart(StringGenerator(), '/', conf)
